283c126a2287e152ea2578d1eb1d22081c73c3a9
export-web <export@piks-l.com>
2026-09-11 17:26:09 +0000

    Export web de 65b0b9d [skip ci]

--- commun/version.gd
+++ commun/version.gd
@@ -2,8 +2,8 @@ class_name Version
 extends RefCounted
 ## ÉCRIT PAR le workflow d'export — ne pas modifier à la main.
 
-const COMMIT := "bf76884"
-const DATE := "11/09/2026 16:05 UTC"
+const COMMIT := "65b0b9d"
+const DATE := "11/09/2026 17:25 UTC"
 
 static func etiquette() -> String:
 	if COMMIT == "sources":
